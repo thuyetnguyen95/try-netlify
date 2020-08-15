e0ca8cc51e52e2fe18e1c72a46f6e0af96f84325
thuyetnv <thuyettiensinh@gmail.com>
2020-08-15 11:27:34 +0700

    Update: build

---
+++ build/precache-manifest.7f3b8c1436aded31d81b8323e1f215c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461c8fe8fac165d7c36c3b08f74eae6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8e7ff4543adfd67505b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a67bf45170f0b9ca7a5",
+    "url": "/static/js/2.c76432dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c76432dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e7ff4543adfd67505b",
+    "url": "/static/js/main.eb9708a6.chunk.js"
+  },
+  {
+    "revision": "59cc729f0c0fc95c17c3",
+    "url": "/static/js/runtime-main.e6948f9a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
